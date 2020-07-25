a6aa55d1f061df6457c7ebb30439e04a23ebf4e6
Anisha Kapoor <anisha1594@gmail.com>
2020-07-26 02:10:41 +0530

    Added traceId

--- src/main/java/com/centime/tasks/helloservice/HelloController.java
+++ src/main/java/com/centime/tasks/helloservice/HelloController.java
@@ -3,6 +3,7 @@ package com.centime.tasks.helloservice;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +11,7 @@ public class HelloController {
 
     @Operation(summary = "Fetch a string 'Hello' wrapped with a spring response entity")
     @GetMapping("/hello")
-    public ResponseEntity<String> getHello() {
+    public ResponseEntity<String> getHello(@RequestHeader("traceId") String traceId) {
         return ResponseEntity.ok("Hello");
     }
 }
